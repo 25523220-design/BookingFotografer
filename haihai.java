5b96cffc0bb9304ff4cac8be6e77362f4106fd27
Luqman-15 <25523226@students.uii.ac.id>
2026-06-08 17:27:16 +0700

    Update haihai.java

--- haihai.java
+++ haihai.java
@@ -1,6 +1,7 @@
 public class haihai {
     public static void main(String[] args) {
         System.out.println("didar halo");
-        System.out.println("ardika lolo")
+        System.out.println("ardika lolo");
+        System.out.println("woibods");
     }
 }
