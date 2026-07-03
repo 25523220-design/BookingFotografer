b2ec716c069a97259b23d3a9deade4cdf42dcde4
25523220-design <25523220@students.uii.ac.id>
2026-07-03 14:34:52 +0700

    Delete haihai.java

--- haihai.java
+++
@@ -1,7 +0,0 @@
-public class haihai {
-    public static void main(String[] args) {
-        System.out.println("didar halo");
-        System.out.println("ardika lolo");
-        System.out.println("woibods");
-    }
-}
